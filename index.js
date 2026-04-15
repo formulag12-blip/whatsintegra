58739a1791a8cd7ac78eb2fa1ecb9a365845a004
formulag12-blip <formulag12@gmail.com>
2026-04-14 22:33:32 -0300

    fix railway porta correta

--- index.js
+++ index.js
@@ -88,3 +88,6 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, '0.0.0.0', () => {
     console.log('🚀 SERVIDOR RODANDO NA PORTA ' + PORT)
 })
+app.get('/', (req, res) => {
+    res.send('Servidor ativo')
+})
